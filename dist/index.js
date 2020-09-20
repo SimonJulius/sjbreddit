79f6f01fc78a191950cf214ccd05d4c6172d3d9d
SimonJulius <simonjulius0511@gmail.com>
2020-09-20 23:17:55 +0100

    Setup completed for the project

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,35 @@
-console.log("index file");
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const core_1 = require("@mikro-orm/core");
+const mikro_orm_config_1 = __importDefault(require("./mikro-orm.config"));
+const express_1 = __importDefault(require("express"));
+const apollo_server_express_1 = require("apollo-server-express");
+const main = () => __awaiter(void 0, void 0, void 0, function* () {
+    const orm = yield core_1.MikroORM.init(mikro_orm_config_1.default);
+    yield orm.getMigrator().up();
+    const app = express_1.default();
+    const apolloServer = new apollo_server_express_1.ApolloServer({});
+    app.get("/", (_, res) => {
+        res.send("dksjvhdfmdajklgld'klkf");
+    });
+    const port = process.env.PORT || 2000;
+    app.listen(port, () => {
+        console.log(`Listening on port ${port}`);
+    });
+});
+main().catch((err) => {
+    console.error(err);
+});
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-console.log("index file");
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+import microConfig from "./mikro-orm.config";
+import express, { Response } from "express";
+import { ApolloServer } from "apollo-server-express";
+import { GraphQLObjectType } from "graphql";
+const main = async () => {
+  const orm = await MikroORM.init(microConfig);
+  await orm.getMigrator().up();
+
+  const app = express();
+  const apolloServer = new ApolloServer({});
+
+  app.get("/", (_, res: Response) => {
+    res.send("dksjvhdfmdajklgld'klkf");
+  });
+
+  const port = process.env.PORT || 2000;
+  app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+  });
+};
+main().catch((err) => {
+  console.error(err);
+});
